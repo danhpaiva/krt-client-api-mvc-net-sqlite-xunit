326551b540f202a5a3aeb0444b37b78255add9ac
Paiva <danpaiva@live.com>
2025-10-26 10:48:21 -0300

    refact: refatorando a classe Program.cs para ficar mais limpa

--- KRT.Cliente.Api/KRT.Cliente.Api/Program.cs
+++ KRT.Cliente.Api/KRT.Cliente.Api/Program.cs
@@ -1,20 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using KRT.Cliente.Api.Data;
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.")));
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+
+builder
+    .Services
+    .AddDbContext<AppDbContext>(options =>
+    options
+    .UseSqlite(builder
+    .Configuration
+    .GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
