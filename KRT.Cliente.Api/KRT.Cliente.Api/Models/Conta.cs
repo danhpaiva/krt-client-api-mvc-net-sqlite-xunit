564d54e4372b69041d0d4f64c40067811a58b0d2
Paiva <danpaiva@live.com>
2025-10-26 10:18:54 -0300

    feat: criada classe de modelo para o dominio de negocio

---
+++ KRT.Cliente.Api/KRT.Cliente.Api/Models/Conta.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KRT.Cliente.Api.Models;
+
+public class Conta
+{
+    [Key]
+    public Guid Id { get; set; } = Guid.NewGuid();
+    [Required(ErrorMessage = "O campo NomeTitular é obrigatorio.")]
+    public string NomeTitular { get; set; } = default!;
+    [Required(ErrorMessage = "O campo CPF é obrigatorio.")]
+    public string CPF { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
+    public DateTime? AtualizadoEm { get; set; }
+    public bool Status { get; set; } = false;
+    public DateTime? DeletadoEm { get; set; }
+}
