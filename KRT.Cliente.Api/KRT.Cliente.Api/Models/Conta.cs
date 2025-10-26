bbcd9c078f49c960093c4e5a6b51f83dc97c48d8
Paiva <danpaiva@live.com>
2025-10-26 12:16:26 -0300

    refact: criando validacoes para clientes

--- KRT.Cliente.Api/KRT.Cliente.Api/Models/Conta.cs
+++ KRT.Cliente.Api/KRT.Cliente.Api/Models/Conta.cs
@@ -9,7 +9,9 @@ public class Conta
     [Required(ErrorMessage = "O campo NomeTitular é obrigatorio.")]
     public string NomeTitular { get; set; } = default!;
     [Required(ErrorMessage = "O campo CPF é obrigatorio.")]
+    [StringLength(11, ErrorMessage = "O campo CPF deve conter 11 caracteres.")]
     public string CPF { get; set; } = default!;
+    [EmailAddress(ErrorMessage = "O campo Email deve ser um endereço de email válido.")]
     public string Email { get; set; } = default!;
     public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
     public DateTime? AtualizadoEm { get; set; }
